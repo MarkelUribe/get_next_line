3084d93177946024410187d40939151a20399af9
MarkelUribe <uribemarkel@gmail.com>
2024-01-16 13:46:35 +0100

    amogu

--- get_next_line.h
+++ get_next_line.h
@@ -6,10 +6,11 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:51 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/16 13:11:10 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/16 13:41:08 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 
 char *get_next_line(int fd);
