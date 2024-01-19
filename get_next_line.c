ff8dd5eda03fb598bee9e16c4536be5f86d6a9a4
MarkelUribe <uribemarkel@gmail.com>
2024-01-19 17:54:42 +0100

    buffer static

--- get_next_line.c
+++ get_next_line.c
@@ -6,88 +6,33 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:19 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/19 12:28:51 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:37:43 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*free_all(char *buffer, char *remanent, char *line)
-{
-	if (buffer)
-		free(buffer);
-	if (remanent)
-		free(remanent);
-	if (line)
-		free(line);
-	return (NULL);
-}
-
-char	*get_line_remanent(char *remanent)
-{
-	static int	last_pos;
-	char		*line;
-	int			i;
-
-	if (!last_pos)
-		last_pos = 0;
-	i = 0;
-	while (remanent[last_pos + i] != '\n' && remanent[last_pos + i] != '\0')
-		i++;
-	line = (char *)malloc(sizeof(char) * i);
-	if (!line)
-	{
-		free (remanent);
-		return (NULL);
-	}
-	ft_strlcpy(line, &remanent[last_pos], i);
-	last_pos += i;
-	if (last_pos >= ft_strlen(remanent))
-		return (NULL);
-	return (line);
-}
-
-char	*read_line(int fd, char *buffer)
-{
-	static char		*remanent;
-	static ssize_t	bytes_read;
-	char			*temp;
-
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
-		return (NULL);
-	buffer[bytes_read] = '\0';
-	if (remanent)
-	{
-		printf("%s", remanent);
-		if (bytes_read > 0)
-		{
-			temp = (char *)malloc(sizeof(char) * ft_strlen(remanent) + 1);
-			ft_strlcpy(temp, remanent, ft_strlen(remanent) + 1);
-			free(remanent);
-			remanent = (char *)malloc(sizeof(char) * (bytes_read + ft_strlen(temp) + 1));
-			ft_strlcpy(remanent, temp, ft_strlen(temp) + 1);
-			free(temp);
-			ft_strlcat(remanent, buffer, bytes_read + 1);
-		}
-		return (get_line_remanent(remanent));
-	}
-	remanent = (char *)malloc(sizeof(char) * bytes_read + 1);
-	ft_strlcat(remanent, buffer, bytes_read + 1);
-	return (get_line_remanent(remanent));
-}
-
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	char	*line;
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	size_t		bytes_read;
 
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!buffer)
-		return (NULL);
-	line = read_line(fd, buffer);
-	free(buffer);
+	//Leer el resto anterior de buffer
+	line = NULL;
+	bytes_read = 1;
+	while(1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(line), NULL);
+		else if (bytes_read == 0)
+			return (line);
+		buffer[bytes_read] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	//Dejar el resto en buffer
 	return (line);
 }
 
@@ -98,12 +43,10 @@ int	main(void)
 	int		file;
 
 	file = open("test.txt", O_RDONLY);
-	s = get_next_line(file);
-	while(s)
+	while((s = get_next_line(file)) != NULL)
 	{
 		printf("%s\n", s);
-		s = get_next_line(file);
+		free(s);
 	}
-	free(s);
 	close(file);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,62 +6,50 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:54 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/18 14:24:50 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/19 16:38:05 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strlen(char *s)
 {
-	int	i;
+	size_t	i;
 
+	if (s == NULL)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	return (i);
 }
 
-size_t	ft_strlcat(char *dst,
-const char *src, size_t dstsize)
+void	ft_memcpy(char *dst, char *src, size_t len)
 {
-	size_t	s;
-
-	s = ft_strlen(src) + ft_strlen(dst);
-	if (dstsize <= ft_strlen(dst))
-		return (ft_strlen(src) + dstsize);
-	while (*dst != '\0')
-	{
-		dst++;
-		dstsize--;
-	}
-	while (dstsize - 1 > 0 && *src)
-	{
-		*dst = *src;
-		dst++;
-		src++;
-		dstsize--;
-	}
-	*dst = '\0';
-	return (s);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	s;
 	size_t	i;
 
 	i = 0;
-	s = ft_strlen(src);
-	if (dstsize < 1)
-		return (s);
-	while (dstsize - 1 > i && src[i] != '\0')
+	while (i < len)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (s);
+}
+
+char	*ft_strjoin(char *line, char *buf)
+{
+	char	*new_line;
+	size_t	line_len;
+	size_t	buf_len;
+
+	line_len = ft_strlen(line);
+	buf_len = ft_strlen(buf);
+	new_line = (char *)malloc(sizeof(char) * (line_len + buf_len + 1));
+	if (new_line == NULL)
+		return (NULL);
+	ft_memcpy(new_line, line, line_len);
+	ft_memcpy(new_line + line_len, buf, buf_len);
+	new_line[line_len + buf_len] = '\0';
+	free(line);
+	return (new_line);
 }
