ec75be5c2c4b83eb3cb711f4f4e953e8055434ae
MarkelUribe <uribemarkel@gmail.com>
2024-01-22 20:06:31 +0100

    me

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,32 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:19 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/19 15:37:43 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/22 20:06:18 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+char	*get_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	line = NULL;
+	i = 0;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	buffer = ft_strjoin(buffer, &buffer[i]);
+	if (i >= ft_strlen(buffer))
+		return (NULL);
+	line = ft_strjoin(line, buffer[i]);
+	if (!line)
+		return (NULL);
+	if (!line)
+		return (NULL);
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
@@ -22,6 +42,12 @@ char	*get_next_line(int fd)
 	//Leer el resto anterior de buffer
 	line = NULL;
 	bytes_read = 1;
+	if (buffer)
+	{
+		line = get_line(buffer);
+		if (line)
+			return (line);
+	}
 	while(1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
@@ -31,6 +57,8 @@ char	*get_next_line(int fd)
 			return (line);
 		buffer[bytes_read] = '\0';
 		line = ft_strjoin(line, buffer);
+		if (!line)
+			return (NULL);
 	}
 	//Dejar el resto en buffer
 	return (line);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:51 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/19 09:28:02 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/22 19:08:28 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,5 @@
 #endif
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcat(char *dst,
-const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *line, char *buf);
