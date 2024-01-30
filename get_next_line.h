7d907071b9a0829e217de18772588b3f50f63aa4
MarkelUribe <uribemarkel@gmail.com>
2024-01-30 11:31:20 +0100

    dana inda pacoko fallo batzuk emateitu baño

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,24 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:09:51 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/30 10:29:48 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/30 11:20:15 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *s);
 void	ft_memcpy(char *dst, char *src, size_t len);
 char	*ft_strjoin(char *line, char *buf);
 char	*ft_strchr(const char *s, int c);
+
+#endif //GET_NEXT_LINE_H
